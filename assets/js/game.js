649f3fba1ea24376a492105165f82123c8b30aad
Ian L <ileach81@gmail.com>
2021-11-09 17:00:04 -0500

    messed with console commands

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,21 @@
-window.alert("This is an alert! JavaScript is running!");
+// creates function named "fight"
+
+function fight() {
+  window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("what is your robot's name?");
+
+// playerName lacks quotes
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10 + 10);
+//this is a string that uses playerName variable
+console.log("our robot's name is " + playerName);
+
+var name = "your name";
+console.log(name);
+
+// fight();

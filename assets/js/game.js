2a2f41dffe4b1081e65c8cba4802b7278452003e
Ian L <ileach81@gmail.com>
2021-11-09 19:32:25 -0500

    added first steps  for game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,21 +1,52 @@
 // creates function named "fight"
+var playerName = window.prompt("what is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-function fight() {
-  window.alert("The fight has begun!");
+// you can also log values like console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function () {
+  window.alert("Welcome to Robot Gladiators!");
+};
+
+fight();
+
+enemyHealth = enemyHealth - playerAttack;
+console.log(
+  playerName +
+    " attacked " +
+    enemyName +
+    " . " +
+    enemyName +
+    " now has " +
+    enemyHealth +
+    " health remaining "
+);
+
+if (enemyHealth <= 0) {
+  window.alert(enemyName + " has died!");
+} else {
+  window.alert(enemyName + " still has " + enemyHealth + " health left.");
 }
 
-var playerName = window.prompt("what is your robot's name?");
+playerHealth = playerHealth - enemyAttack;
+console.log(
+  enemyName +
+    " attacked " +
+    playerName +
+    " . " +
+    playerName +
+    " now has " +
+    playerHealth +
+    " health remaining "
+);
 
-// playerName lacks quotes
-console.log(playerName);
-
-console.log("This logs a string, good for leaving yourself a message");
-//this will do math and log 20
-console.log(10 + 10);
-//this is a string that uses playerName variable
-console.log("our robot's name is " + playerName);
-
-var name = "your name";
-console.log(name);
-
-// fight();
+if (playerHealth <= 0) {
+  window.alert(playerName + " has died!");
+} else {
+  window.alert(playerName + " still has " + playerHealth + " health left.");
+}
